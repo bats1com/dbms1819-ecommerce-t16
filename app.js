4b4d8b25dd885271fb7611ce39172572d9c95eee
Jomar <jdvista96@gmail.com>
2018-08-03 12:43:06 +0800

    Migrate old products table to products_m1 for module 1  db

--- app.js
+++ app.js
@@ -32,13 +32,13 @@ client.connect()
 	})
 
 /*
-CREATE TABLE Products(id SERIAL PRIMARY KEY, name varchar(80), type varchar(80), description varchar(300), brand varchar(80), price float(2), pic varchar(80));
-INSERT INTO Products(name, type, description, brand, price, pic) VALUES('Genesis', 'Custom In-Ear Monitor', 'FlipEars Genesis is a single balanced armature driver custom in-ear monitors. It is an amazing entry-level CIEM, it is incomparable to other single driver IEMs.', 'Flipears', 8, '/genesis.jpg');
-INSERT INTO Products(name, type, description, brand, price, pic) VALUES('Aiden', 'Custom In-Ear Monitor', 'FlipEars Aiden is a dual-driver custom in-ear monitor. This affordable CIEM is fit for audiophiles and musicians looking for aggressive and full sound with brilliant high frequency.', 'Flipears', 15,'/aiden.jpg');
-INSERT INTO Products(name, type, description, brand, price, pic) VALUES('Trident', 'Custom In-Ear Monitor', 'Trident features elevated low-end and treble regions for a refined take on what would normally be considered a pop sound. Equipped with 3 Knowles balanced armature drivers per side.', 'Noble Audio', 30,'/trident.jpg');
-INSERT INTO Products(name, type, description, brand, price, pic) VALUES('A6t', 'Custom In-Ear Monitor', 'The A6t builds off of our exceptional 6-driver design that quickly became the “in-ear monitor of choice” for touring musicians and discerning music lovers.', '64 Audio', 65,'/a6t.jpg');
-INSERT INTO Products(name, type, description, brand, price, pic) VALUES('Legend X', 'Custom In-Ear Monitor', 'From its arsenal of 7 proprietary drivers to its industry leading, state-of-the-art 10-way synX crossover network, the Legend X is the culmination of everything extraordinary we do at Empire.', 'Empire Ears', 115,'/legend-x.jpg');
-INSERT INTO Products(name, type, description, brand, price, pic) VALUES('Layla', 'Custom In-Ear Monitor', 'Layla is perfection, perfected. This IEM will replicate detail of instruments in a way that makes it feel like the musicians are in the room with you. Layla uses custom-made Pproprietary Balanced Armature Drivers with a 12 Driver configuration.', 'JH Audio', 150,'/layla.jpg');
+CREATE TABLE products_m1(id SERIAL PRIMARY KEY, name varchar(80), type varchar(80), description varchar(300), brand varchar(80), price float(2), pic varchar(80));
+INSERT INTO products_m1(name, type, description, brand, price, pic) VALUES('Genesis', 'Custom In-Ear Monitor', 'FlipEars Genesis is a single balanced armature driver custom in-ear monitors. It is an amazing entry-level CIEM, it is incomparable to other single driver IEMs.', 'Flipears', 8, '/genesis.jpg');
+INSERT INTO products_m1(name, type, description, brand, price, pic) VALUES('Aiden', 'Custom In-Ear Monitor', 'FlipEars Aiden is a dual-driver custom in-ear monitor. This affordable CIEM is fit for audiophiles and musicians looking for aggressive and full sound with brilliant high frequency.', 'Flipears', 15,'/aiden.jpg');
+INSERT INTO products_m1(name, type, description, brand, price, pic) VALUES('Trident', 'Custom In-Ear Monitor', 'Trident features elevated low-end and treble regions for a refined take on what would normally be considered a pop sound. Equipped with 3 Knowles balanced armature drivers per side.', 'Noble Audio', 30,'/trident.jpg');
+INSERT INTO products_m1(name, type, description, brand, price, pic) VALUES('A6t', 'Custom In-Ear Monitor', 'The A6t builds off of our exceptional 6-driver design that quickly became the “in-ear monitor of choice” for touring musicians and discerning music lovers.', '64 Audio', 65,'/a6t.jpg');
+INSERT INTO products_m1(name, type, description, brand, price, pic) VALUES('Legend X', 'Custom In-Ear Monitor', 'From its arsenal of 7 proprietary drivers to its industry leading, state-of-the-art 10-way synX crossover network, the Legend X is the culmination of everything extraordinary we do at Empire.', 'Empire Ears', 115,'/legend-x.jpg');
+INSERT INTO products_m1(name, type, description, brand, price, pic) VALUES('Layla', 'Custom In-Ear Monitor', 'Layla is perfection, perfected. This IEM will replicate detail of instruments in a way that makes it feel like the musicians are in the room with you. Layla uses custom-made Pproprietary Balanced Armature Drivers with a 12 Driver configuration.', 'JH Audio', 150,'/layla.jpg');
 */
 //View engine setup
 const app = express();
@@ -51,7 +51,7 @@ app.use(bodyParser.json());
 
 
 app.get('/', function(req,res) {
-	client.query('SELECT * FROM Products', (req, data)=>{
+	client.query('SELECT * FROM products_m1', (req, data)=>{
 		var list = [];
 		for (var i = 0; i < data.rows.length; i++) {
 			list.push(data.rows[i]);
@@ -65,7 +65,7 @@ app.get('/', function(req,res) {
 
 app.get('/products/:id', (req,res)=>{
 	var id = req.params.id;
-	client.query('SELECT * FROM Products', (req, data)=>{
+	client.query('SELECT * FROM products_m1', (req, data)=>{
 		var list = [];
 		for (var i = 0; i < data.rows.length+1; i++) {
 			if (i==id) {
@@ -129,7 +129,7 @@ app.post('/products/:id/send', function(req, res) {
         console.log('Message sent: %s', info.messageId);
         console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
-        client.query('SELECT * FROM Products', (req, data)=>{
+        client.query('SELECT * FROM products_m1', (req, data)=>{
 			var list = [];
 			for (var i = 0; i < data.rows.length+1; i++) {
 				if (i==id) {
